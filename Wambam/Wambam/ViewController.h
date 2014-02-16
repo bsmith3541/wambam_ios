dfda74e9ca4eb554384be0754aec80e69a51cca1
Brandon Smith <brandon.smith@yale.edu>
2014-02-16 14:47:42 -0500

    updated UI buttons due to naming convention issue

--- Wambam/Wambam/ViewController.h
+++ Wambam/Wambam/ViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UIButton *newTaskButton;
+@property (weak, nonatomic) IBOutlet UIButton *createTaskButton;
 @property (weak, nonatomic) IBOutlet UIButton *fulfillTaskButton;
 
 
