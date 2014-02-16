bbdbcb68d2782343c620aebbc0354638e5ed8bab
Brandon Smith <brandon.smith@yale.edu>
2014-02-16 14:41:19 -0500

    added UI button connections to header file

--- Wambam/Wambam/ViewController.h
+++ Wambam/Wambam/ViewController.h
@@ -9,5 +9,8 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIButton *newTaskButton;
+@property (weak, nonatomic) IBOutlet UIButton *fulfillTaskButton;
+
 
 @end
