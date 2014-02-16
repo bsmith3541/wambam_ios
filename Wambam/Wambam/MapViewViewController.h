8d23d978da1b70778f95a1bcfa0c5669cd688790
Brandon Smith <brandon.smith@yale.edu>
2014-02-16 15:20:49 -0500

    created new MapViewController class to handle the map view

---
+++ Wambam/Wambam/MapViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewViewController.h
+//  Wambam
+//
+//  Created by Brandon Smith on 2/16/14.
+//  Copyright (c) 2014 Wambam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreLocation/CoreLocation.h>
+@interface MapViewViewController : UIViewController
+
+@end
---
+++ Wambam/Wambam/MapViewViewController.m
@@ -0,0 +1,38 @@
+//
+//  MapViewViewController.m
+//  Wambam
+//
+//  Created by Brandon Smith on 2/16/14.
+//  Copyright (c) 2014 Wambam. All rights reserved.
+//
+
+#import "MapViewViewController.h"
+
+@interface MapViewViewController ()
+
+@end
+
+@implementation MapViewViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
