f76d99275c29786a9bd0e6595015571320912fc7
Brandon Smith <brandon.smith@yale.edu>
2014-02-23 14:31:35 -0500

    updated code to allow for infoWindows for markers...class NOT completely implemented

---
+++ Wambam/Wambam/InfoWindow.h
@@ -0,0 +1,13 @@
+//
+//  InfoWindow.h
+//  Wambam
+//
+//  Created by Brandon Smith on 2/19/14.
+//  Copyright (c) 2014 Wambam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoWindow : UIView
+
+@end
---
+++ Wambam/Wambam/InfoWindow.m
@@ -0,0 +1,31 @@
+//
+//  InfoWindow.m
+//  Wambam
+//
+//  Created by Brandon Smith on 2/19/14.
+//  Copyright (c) 2014 Wambam. All rights reserved.
+//
+
+#import "InfoWindow.h"
+
+@implementation InfoWindow
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
